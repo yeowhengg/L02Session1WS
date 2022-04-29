9f0152dae53af52151aa664062df521bfa79f37b
Yeow Heng <20042741@myrp.edu.sg>
2022-04-29 15:07:21 +0800

    Session 2 Exercise 1

---
+++ app/src/main/java/sg/edu/rp/c346/id20042741/demomysecondapp/MainActivity_sess2_Exercise1.java
@@ -0,0 +1,14 @@
+package sg.edu.rp.c346.id20042741.demomysecondapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity_sess2_Exercise1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_sess2__exercise1);
+    }
+}
