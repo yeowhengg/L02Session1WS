2e584ddfed548b3b50336b5e83b813bbaccf4204
Yeow Heng <20042741@myrp.edu.sg>
2022-04-29 12:50:40 +0800

    Add in yellow color in colors.xml; Add in text in strings.xml; set parent weight to 2, set 2 linearlayout parent weight to 1 each

---
+++ app/src/main/java/sg/edu/rp/c346/id20042741/demomysecondapp/MainActivity_Exercise2B.java
@@ -0,0 +1,14 @@
+package sg.edu.rp.c346.id20042741.demomysecondapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity_Exercise2B extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main__exercise2_b);
+    }
+}
